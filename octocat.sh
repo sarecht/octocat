38e35a60ebab1ba453c4a2cee54cc6521372e915
Sanjana <sanjkris@live.unc.edu>
2016-10-31 20:41:35 +0000

    Wrote questions in script

---
+++ octocat.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Say hello
+echo "Hello, I have questions for you!"
+
+#Ask for a name
+echo "What is your name?"
+read varname
+
+#Ask for age
+echo "How old are you?"
+read varage
+echo "You are $varage years old!"
+
+#Ask for class standing
+echo "What is your class standing (freshman, grad, etc.)?"
+read varclass
+echo "Your class standing is $varclass!"
+
+#Ask for favorite animal
+echo "What is your favorite animal?"
+read varanim
+echo "Your favorite animal is $varanim!"
+
+#Ask for favorite color
+echo "What is your favorite color?"
+read varcol
+echo "Your favorite color is $varcol!"
+
+#Say goodbye
+echo "Thank you for your answers! Goodbye!"
