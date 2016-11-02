2ce7e1662d790869cb3dc632de85d39072462585
Ellen <eclinester@gmail.com>
2016-11-02 20:46:44 +0000

    added: timestamp, unique identifying string, appended all data to data.csv

--- octocat.sh
+++ octocat.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Defining variables in the beginning: making unique identifiers for each user, creating variables for timestamp
+ID=`python -c 'import uuid; print str(uuid.uuid1())'`
+time=`(date)`
 
 #Say hello
 echo "Hello, I have questions for you!"
@@ -27,5 +30,18 @@ echo "What is your favorite color?"
 read varcol
 echo "Your favorite color is $varcol!"
 
+#Give the date
+printf "Today is %s\n" "$(date)"
+
+#Print unique identifier last
+echo "User ID = $ID"
+
+answers=`echo "$varname,$varage,$varclass,$varanim,$varcol,$(date),$ID"`
+echo "$answers" >> data.csv
+
 #Say goodbye
 echo "Thank you for your answers! Goodbye!"
+
+
+
+
